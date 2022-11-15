84b0193b3bb87cbde7ce2f7ab950ea58c99ddafe
Sahil Chaudhari <76389534+Lord2709@users.noreply.github.com>
2022-11-15 17:44:23 +0530

    Fetched team data from teams.txt

--- tut08/tut08.py
+++ tut08/tut08.py
@@ -1,10 +1,32 @@
+import os 
+os.system('cls')
+import re
+import numpy as np
+import math
 
 from datetime import datetime
 start_time = datetime.now()
 
 #Help
 def scorecard():
-	pass
+	file1 = open("Scorecard.txt","w")
+
+	list_of_files = ['teams.txt','pak_inns1.txt','india_inns2.txt']
+
+	team_names = []
+	team = {}
+	with open(list_of_files[0]) as f:
+		content = f.readlines()
+		for line in content:
+			if len(re.split(": ",line)) != 1:
+				if re.split(r"\s+",line,1)[0] not in team:
+					team[re.split(r"\s+",line,1)[0]] = 0
+				
+				team_member = re.split(", ",(re.split(": ",line)[1]))
+				team_member[len(team_member)-1] = team_member[len(team_member)-1][:-1]
+				team[re.split(r"\s+",line,1)[0]] = team_member
+				team_names.append(re.split(r"\s+",line,1)[0])
+	print(team_names,team)
 
 
 ###Code
